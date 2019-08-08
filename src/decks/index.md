3d256b81a13391c1b954febfc294dbcdea8cb6f6
amazingandyyy <andycandomore@gmail.com>
2019-08-08 16:44:45 -0700

    markdown to presentation

---
+++ src/decks/index.md
@@ -0,0 +1,60 @@
+# Speedeck
+
+## speed up your decks
+
+### Markdown as a Presentation
+
+Andy Chen
+Aug 7, 2019
+
+Andy Chen
+Aug 7, 2019
+
+Andy Chen
+Aug 7, 2019
+
+Andy Chen
+Aug 7, 2019
+
+------
+
+# Andy
+
+![child](https://user-images.githubusercontent.com/7886068/62680348-e1a77c80-b96b-11e9-8cc9-b651ad17034f.jpg)
+- Graphic designer turned Anthropologist turned frontend turned infrastructure engineer
+- Udemy-as-a-university, afternoon qualification, keep-you-posted
+- live in Mtn view, Fremont, Berkeley, Hayward, Daly City and finally made up in SF
+
+------
+
+# I love Wafflejs since July 2015
+
+![wafflejs](https://wafflejs.com/images/social-2019-08.jpg)
+Used 20 student FREE tickets
+
+finally be student sponsor since April 2019 :)
+
+My first speech here :)
+
+I also mentor at Nodeschool SF/OAK
+
+------
+
+# Live Demo
+
+------
+
+# It's open source on the hub of git
+
+https://github.com/amazingandyyy/speedeck
+
+------
+
+# Thank you
+
+```js
+website: 'https://amazingandyyy.com'
+email: 'amazingandyyy@gmail.com',
+twitter: 'https://twitter.com/amazingandyyyer',
+github: 'https://github.com/amazingandyyy'
+```
--- src/up.js
+++ src/up.js
@@ -1,29 +1,67 @@
-import path from 'path'
+import path from 'path';
+import fs from 'fs';
+import showdown from 'showdown';
 const exec = require('child_process').exec
-import {Printer} from './util'
+import { readFile } from 'fs';
+import {Printer} from './util';
 
-const up = (flags) => {
-  const {src} = flags
-  const srcFolder = src?src:'decks'
+const reset = (path) => {
+  console.log(`reset ${path}`)
+  exec(`rm -rf ${path}`)
+  exec(`mkdir -p ${path}`)
+}
+const up = ({src, theme}) => {
+  theme = theme ? theme : 'basic';
+  const srcFolder = src?src:'decks';
   const srcFolderPath = path.resolve(__dirname, srcFolder)
-  const content = require(`${srcFolderPath}/content`)
-  const cacheFoler = path.resolve(__dirname, '.cache.decks')
-  const {presentation, author} = content.default;
-  // TODO: don't just remove file
-  exec(`rm -rf ${path.resolve(__dirname, '.cache.decks/**')}`)
-  const cover = new Printer(path.join(cacheFoler, '00.html'))
-  console.log(presentation.cover.title)
-  cover.print(`<html>
-  <head>
-    <title>${presentation.cover.title}</title>
-  </head>
-  <body>
-    <h1>${presentation.cover.title}</h1>
-    <h3>${presentation.cover.subtitle}</h3>
-    <p>${presentation.cover.time}</p>
-  </body>
-  </html>`.trim())
-  cover.end()
+  const tempateFolderPath = path.resolve(__dirname, 'templates')
+  const cacheFoler = path.join(__dirname, '.cache.decks')
+  reset(cacheFoler)
+  exec(`cp -rf ${tempateFolderPath}/themes/${theme}.css ${cacheFoler}`)
+  exec(`cp -rf ${tempateFolderPath}/speedeck.js ${cacheFoler}`)
+  const converter = new showdown.Converter()
+  fs.readFile(`${srcFolderPath}/index.md`, 'utf8', (err, data) => {
+    const html = converter.makeHtml(data);
+    const pages = html.split('<hr />')
+    pages.forEach((content, i)=>{
+      const file = new Printer(path.join(cacheFoler, `${i}.html`))
+      file.print(`<html>
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <meta property="og:type" content="website">
+          <meta property="og:image" content="">
+          <meta property="og:site_name" content="">
+          <title>Presentation | ${i}</title>
+          <link rel='stylesheet' type='text/css' href='${theme}.css'>
+        </head>
+        <body>
+          <div id='displayer'>
+            ${content}
+          </div>
+          <div>
+            <a id='btn-last-page' href='${i-1}.html'>last page</a>
+            <a id='btn-next-page' href='${i+1}.html'>next page</a>
+            <a id='btn-fullscreen' onclick='toggleFullScreen()'>full screen</a>
+          </div>
+          <script type="application/javascript" src='speedeck.js' />
+        </body>
+        </html>`)
+      file.end()
+      console.log(i, 'done')
+    })
+  });
+  // cover.print(`<html>
+  // <head>
+  //   <title>${presentation.cover.title}</title>
+  // </head>
+  // <body>
+  //   <h1>${presentation.cover.title}</h1>
+  //   <h3>${presentation.cover.subtitle}</h3>
+  //   <p>${presentation.cover.time}</p>
+  // </body>
+  // </html>`.trim())
+  // cover.end()
 }
 
 up({src: 'decks'})
